5254d32093c0ff971d1d8ae15343ed19d25b46dc
Martin Salinas <ssalinasmartin@gmail.com>
2026-01-11 18:55:28 +0100

    Add CMakeLists file (#9)
    This PR will not compile until there is actual source code in src
    folder. It can be merged now bypassing checks which should work when
    code is added.

--- tests/unitary/src/geometry/test_axis_3d.cpp
+++ tests/unitary/src/geometry/test_axis_3d.cpp
@@ -3,7 +3,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 
-#include <xmipp4/core/axis_3d.hpp>
+#include <xmipp4/em/geometry/axis_3d.hpp>
 
 using namespace xmipp4;
 
